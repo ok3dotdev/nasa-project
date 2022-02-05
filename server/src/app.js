8ca4a7f0070b60796dcef609d27f6ec40cc6bf6b
Henry Oke <hoke@LMG-00053.local>
2022-02-05 15:51:46 -0500

    Initial commit

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+const cors = require('cors');
+const path = require('path');
+const express = require('express');
+
+const planetsRouter = require('./routes/planets/planets.router');
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(planetsRouter);
+
+module.exports = app;
