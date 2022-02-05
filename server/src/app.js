e8f3ee7eaa611c11fd52305a4e4651c9a012bb68
Henry Oke <hoke@LMG-00053.local>
2022-02-05 18:30:43 -0500

    index.html included

--- server/src/app.js
+++ server/src/app.js
@@ -12,5 +12,8 @@ app.use(cors({
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRouter);
+app.get('/', (req, res)=>{
+    res.send(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 module.exports = app;
